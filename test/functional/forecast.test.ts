b4dee47e9ef25d90dc56375c921e18cfde36f520
Pedro Dalpa <pedro.dalpa@gmail.com>
2025-01-22 14:50:09 -0300

    Add first e2e tests

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,11 +1,25 @@
-import supertest from 'supertest'
-import { App } from 'supertest/types'
+
 
 describe('Beach forecast functional tests', () => {
   it('should return a forecast with just a few times', async () => {
-    const app:App = {} as App
-    const { body, status } = await supertest(app).get('/forecast')
+    const { body, status } = await global.testRequest.get('/forecast')
     expect(status).toBe(200)
-    expect(body).toBeInstanceOf(Array)
+    expect(body).toEqual([{
+      'time': '2020-04-26T00:00:00+00:00',
+      'forecast': [{
+        'lat': -33.792726,
+        'lng': 151.289824,
+        'name': 'Manly',
+        'position': 'E',
+        'rating': 2,
+        'swellDirection': 64.26,
+        'swellHeight': 0.15,
+        'swellPeriod': 3.89,
+        'time': '2020-04-26T00:00:00+00:00',
+        'waveDirection': 231.38,
+        'waveHeight': 0.47,
+        'windDirection': 299.45
+      }]}
+    ])
   })
 })
---
+++ test/globals.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-var */
+declare global {
+  var testRequest: import('supertest/lib/agent')
+}
+
+export {}
