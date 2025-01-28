4a3178bdae712c30cd8feb8b5a4d880f8cc05d58
Pedro Dalpa <pedro.dalpa@gmail.com>
2025-01-28 10:16:09 -0300

    Add error handling for beach create

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,6 +1,7 @@
 import { Controller, Post } from '@overnightjs/core'
 import { Beach } from '@src/models/beach'
 import { Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 @Controller('beaches')
 export class BeachController {
@@ -9,9 +10,17 @@ export class BeachController {
     req: Request,
     res: Response
   ): Promise<void> {
-    const beach = new Beach(req.body)
-    const result = await beach.save()
+    try {
+      const beach = new Beach(req.body)
+      const result = await beach.save()
 
-    res.status(201).send(result.toJSON())
+      res.status(201).send(result.toJSON())
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message })
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' })
+      }
+    }
   }
 }
